bf9961a6fb70c431a4fe5d5561ee811fbbaff1a6
Maksim Lin <maks@manichord.com>
2022-03-24 12:30:57 +1100

    package scaffold with ffi generated binding

---
+++ example/dart_sunvox_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_sunvox/dart_sunvox.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_sunvox.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_sunvox;
+
+export 'src/dart_sunvox_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
