e0020efcea426f5266cd349ae3867d0001fd93e9
Maksim Lin <maks@manichord.com>
2022-09-18 18:03:29 +1000

    basic file playback example working

--- example/dart_sunvox_example.dart
+++ example/dart_sunvox_example.dart
@@ -1,8 +1,23 @@
 import 'package:dart_sunvox/dart_sunvox.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   final sunvox = LibSunvox();
   final v = sunvox.versionString();
   print('sunvox lib version: $v');
+
+  const filename = "sunvox_lib/resources/song01.sunvox";
+  await sunvox.load(filename);
+  // or as data using Dart's file ops
+  // final data = File(filename).readAsBytesSync();
+
+  sunvox.volume = 256;
+
+  sunvox.play();
+  print("playing:$filename ...");
+
+  await Future<void>.delayed(Duration(seconds: 5));
+
+  sunvox.stop();
+
   sunvox.shutDown();
 }
--- lib/src/dart_sunvox_base.dart
+++ lib/src/dart_sunvox_base.dart
@@ -1,4 +1,6 @@
 import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:dart_sunvox/src/libsunvox_generated_bindings.dart';
 import 'package:ffi/ffi.dart';
@@ -22,8 +24,6 @@ class LibSunvox {
   }
 
   String versionString() {
-    final version = _init();
-
     final major = (version >> 16) & 255;
     final minor1 = (version >> 8) & 255;
     final minor2 = (version) & 255;
@@ -35,6 +35,38 @@ class LibSunvox {
     final config = calloc<Int8>();
     return _sunvox.sv_init(config, 44100, 2, 0);
   }
+
+  /// Load sunvox file
+  Future<void> load(String filename) async {
+    final namePtr = filename.toNativeUtf8().cast<Int8>();
+    final result = _sunvox.sv_load(0, namePtr);
+    if (result != 0) {
+      throw Exception("cannot load file: $filename error code:$result");
+    }
+    // don't forget to free the pointer created by toNativeUtf8
+    // TODO: use arena allocator
+    calloc.free(namePtr);
+  }
+
+  /// Load project file as binary data
+  Future<void> loadData(Uint8List data) async {   
+    final Pointer<Uint8> sData = calloc<Uint8>(data.length); // Allocate a pointer large enough.
+    for (int i = 0; i < data.length; i++) {
+      sData[i] = data[i];
+    }
+    final result = _sunvox.sv_load_from_memory(0, sData.cast(), data.length);
+
+    if (result != 0) {
+      throw Exception("cannot load data, error code:$result");
+    }
+  }
+
+  int play() => _sunvox.sv_play_from_beginning(0);
+
+  void stop() => _sunvox.sv_stop(0);
+
+  // volume 0 to 256
+  set volume(int v) => _sunvox.sv_volume(0, v);
 
   void shutDown() {
     _sunvox.sv_close_slot(0);
